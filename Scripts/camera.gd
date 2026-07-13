ce829f3aeb78bf3bbcaefc0247f1a2b0d0e1821c
angelo <angelombritovieira@gmail.com>
2026-07-13 13:37:43 -0300

    adição de cenas e troca de nós
    adição da camera como cena pra ela seguir o jogador,criação da cena  de final de fase feita para ficar trocando de fase e troca de alguns nó filhos para nó pai

---
+++ Scripts/camera.gd
@@ -0,0 +1,22 @@
+extends Camera2D
+
+var alvo = Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	descobrirPosicaoPlayer()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x = alvo.position.x
+	position.y = alvo.position.y
+
+func descobrirPosicaoPlayer():
+	var nosPlayer = get_tree().get_nodes_in_group("Player")
+	
+	if nosPlayer.size() == 0:
+		push_error("Não tem player na cena")
+		return 
+		
+	alvo = nosPlayer[0]
---
+++ Scripts/final_de_fase.gd
@@ -0,0 +1,53 @@
+extends Area2D
+@onready var zona_inicial: Node2D = $"../.."
+@onready var zona_cyber: Node2D = $"../../../ZonaCyber"
+@onready var zona_tema: Node2D = $"../../../ZonaTema"
+@onready var zona_boss_final: Node2D = $"../../../ZonaBossFinal"
+
+var zonas = {
+	zonaInicial = $"../..",
+	zonaCyber = $"../../../ZonaCyber",
+	zonaTema = $"../../../ZonaTema",
+	zonaBossFinal = $"../../../ZonaBossFinal"
+}
+
+var noAtual = Node2D
+var proxNo = Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	verificarZonaAtual()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	trocarFase(noAtual,proxNo)
+	pass
+	
+func trocarFase(noAntigo: Node2D,proxNo: Node2D):
+	var noPai = noAntigo.get_parent()
+	
+	if noPai:
+		noPai.add_child(proxNo)
+		noPai.remove_child(noAntigo)
+		noAntigo.queue_free()
+
+func verificarZonaAtual():
+	var caminhoAtual = String(get_path())
+	
+	for i in zonas:
+		if caminhoAtual.has(String(i)):
+			caminhoAtual = i
+	
+	var noZona = caminhoAtual.add_child
+	
+	
+	
+	
+	
+	
+	
