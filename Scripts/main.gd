06a64ca89ee198ef6119705760bb23f28e17f97e
angelo <angelombritovieira@gmail.com>
2026-07-13 22:58:15 -0300

    alterações do main e retirada de uma cena que não irá usar

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,6 +4,11 @@ extends Node2D
 @onready var zonaTema: Node2D = $ZonaTema
 @onready var zonaBossFinal: Node2D = $ZonaBossFinal
 
+var grupos = {
+	"0" = "ZonaCyber",
+	"1" = "ZonaMagia",
+	"2" = "zonaTema"
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,19 +21,24 @@ func _process(delta: float) -> void:
 
 func mudarDeFase(numFase:int,zonaAtual:String):
 	var nosZonaAtual = get_tree().get_nodes_in_group(zonaAtual)
-	get_tree().change_scene_to_node(nosZonaAtual[numFase + 1])
-
+	
 	var proximaFase = numFase + 1
 	if proximaFase < nosZonaAtual.size():
-		var novaFase = nosZonaAtual[proximaFase]
-		
-		if novaFase.get_parent() != null:
-			novaFase.get_parent().remove_child(novaFase)
-			
-		var cenaAtual = get_tree().root
-		get_tree().current_scene.queue_free()
-		cenaAtual.add_child(novaFase)
-		get_tree().current_scene = novaFase
-		
+		trocarFase(proximaFase,nosZonaAtual)
+	else:
+		zonaAtual = grupos[str(randi_range(0,2))]
+		nosZonaAtual = get_tree().get_nodes_in_group(zonaAtual)
+		trocarFase(0,nosZonaAtual)
+	
 func mudarDeZona(zonaAtual:String):
 	get_tree().change_scene_to_file("res://"+ zonaAtual +".tscn")
+
+func trocarFase(proximaFase:int,nosZonaAtual):
+	var novaFase = nosZonaAtual[proximaFase]
+	if novaFase.get_parent() != null:
+		novaFase.get_parent().remove_child(novaFase)
+		
+	var cenaAtual = get_tree().root
+	get_tree().current_scene.queue_free()
+	cenaAtual.add_child(novaFase)
+	get_tree().current_scene = novaFase
